b59e83d8c8cae188349a4f3468b4e84662afd3b2
innocentEdosa <ilegbinijieinnocentcareer@gmail.com>
2018-12-20 16:00:14 +0100

    Create user profile page

---
+++ UI/js/main.js
@@ -0,0 +1,11 @@
+// modal to create post
+var createPostLink1 = document.getElementById('createlink1');
+var  popup = document.getElementById('popup');
+
+createPostLink1.addEventListener('click',function(){
+    popup.style.display= "block";
+   popup.style.opacity = "1";
+   
+});
+
+
