0c7f989860e63ac866fa3cc8cd80fe5894a44912
innocentEdosa <ilegbinijieinnocentcareer@gmail.com>
2018-12-20 16:14:43 +0100

    Create admin page

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,11 +1,23 @@
+
 // modal to create post
 var createPostLink1 = document.getElementById('createlink1');
+var createPostLink2 = document.getElementById('createlink2');
 var  popup = document.getElementById('popup');
 
-createPostLink1.addEventListener('click',function(){
-    popup.style.display= "block";
-   popup.style.opacity = "1";
-   
+createPostLink1.addEventListener('click', function(){
+    popup.style.display = "block";
+    popup.style.opacity = "1";
+
+});
+
+createPostLink2.addEventListener('click', function(){
+    popup.style.display = "block";
+    popup.style.opacity = "1";
+});
+
+var closebtn  = document.getElementById('close');
+closebtn.addEventListener('click',function(){
+    popup.style.display = "none";
 });
 
 
